f22a301f03664720f4b913bd9c769b8f23f70cf4
Tiger Nie <nhl0819@hotmail.com>
2019-10-23 17:11:29 -0500

    Implemented current features with REST API

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 #!/usr/bin node
-const express = require('express')
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const sqlite3 = require('sqlite3').verbose();
 const moment = require('moment');
+const uuidv1 = require('uuid/v1')
 
-
-// Initialise https Server and home page
-http.listen(6969, function() {
-    console.log('listening on *:6969')
-});
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
-});
-app.use(express.static(__dirname));
+const express = require('express')
+const session = require('express-session')
+const app = express();
+const http = require('http').createServer(app);
 
 
 // Connect to the database
@@ -26,52 +18,57 @@ let db = new sqlite3.Database('data/tiger-home.db', (err) => {
 });
 
 
-// Socket.io setup
-io.on('connection', function(socket) {
-    console.log('a user connected');
+// Initialise https Server and home page
+http.listen(6969, function() {
+    console.log('listening on *:6969')
+});
+app.use(express.static(__dirname));
 
-    // get latest datafile and parse
-    socket.on('get-new-data', function(){
-
+function parseDB(count) {
+    return new Promise(function(resolve, reject) {
+        // Parse DB Data
+        var cmd = `SELECT timestamp, temperature, activity FROM sensor_data ORDER BY timestamp DESC LIMIT ${count}`;
         db.serialize(() => {
             let sensorData = {
                 time: [],
                 temperature: [],
                 activity: [],
             };
-            db.each("SELECT timestamp, temperature, activity FROM sensor_data ORDER BY timestamp DESC LIMIT 1500",
-                function (err, row) {
+            db.each(cmd, function (err, row) {
                     if (err) { console.error(err.message); }
-                    
                     sensorData.time.push(moment(row.timestamp * 1000).format('MM/DD/YY HH:mm'));
                     sensorData.temperature.push(row.temperature);
                     sensorData.activity.push(row.activity);
                 }, function () {
-                    socket.emit('update-data', sensorData);
-                });
-        });
+                    // Do something with sensorData
+                    resolve(sensorData);
+                }
+            );
+        })
     })
+}
 
-    socket.on('get-back-data', function() {
-        // Do something here to parse old data
-        socket.emit('update-data', sensorData);
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/index.html')
+});
+app.get('/home_api', (req, res) => {
+    return res.send("Get request to /home_api, uuid: "+uuidv1())
+})
+app.get('/home_api/get_curr_data', async function(req, res) {
+    return parseDB(1000).then(function(result) {
+        res.send(result);
+    }, function(err) {
+        console.log(err)
     })
-    
-    socket.on('get-next-data', function() {
-        // Do something here to parse new data
-        socket.emit('update-data', sensorData);
+})
+app.get('/home_api/get_curr_data/:num', async function(req, res) {
+    return parseDB(req.params.num).then(function(result) {
+        res.send(result);
+    }, function(err) {
+        console.log(err)
     })
-    
-    socket.on('acknowledge-data', function (data) {
-        console.log(data);
-    });
-
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
-
-
-    // socket.on('chat message', function(msg) {
-    //    console.log('message: ' + msg);
-    // });
+})
+app.get('/emergency/:id', function (req, res, next) {
+    var id = req.params.id;
+    console.log('The id: ' + id);
 });
--- static/js/main-client.js
+++ static/js/main-client.js
@@ -1,53 +1,33 @@
 // // Plots
 // // TODO: Add navigation between dates.
 
-var debug_;
+var cacheSensorData;
+
+function get_data() {
+  var url = "/home_api/get_curr_data";
+  $.ajax({
+    url: url,
+    type: 'GET',
+    async: true,
+    data: null,
+    success: (sensorData) => {
+      cacheSensorData = sensorData;
+      // Update cards
+      var len = sensorData.time.length-1;
+      var str = sensorData.temperature[len] + " C"
+      document.getElementById('temperatureStatus').innerHTML = str;
+      var str = sensorData.activity[len];
+      document.getElementById('activityStatus').innerHTML = str;
+
+      // Update plots
+      areaPlot('graphTemperature', sensorData.time, sensorData.temperature, 'Temperature (°C)')
+      areaPlot('graphActivity', sensorData.time, sensorData.activity, 'Activity (count)')
+    }
+  })
+}
+
 $(function() {
   "use strict";
+  get_data();
 
-  // Socket Client
-  var socket = io();
-  socket.on('update-data', function (sensorData) {
-    console.log("Received new data");
-    debug_ = sensorData;
-
-    // Update cards
-    var len = sensorData.time.length-1;
-    var str = sensorData.temperature[len] + " C"
-    document.getElementById('temperatureStatus').innerHTML = str;
-    var str = sensorData.activity[len];
-    document.getElementById('activityStatus').innerHTML = str;
-
-    // Update plots
-    areaPlot('graphTemperature', sensorData.time, sensorData.temperature, 'Temperature (°C)')
-    areaPlot('graphActivity', sensorData.time, sensorData.activity, 'Activity (count)')
-
-    // Acknowledge data to the server
-    socket.emit('acknowledge-data', { my:'index.html received data!'});
-  });
-
-  socket.emit('get-new-data');
-  console.log("asking for data");
-
-  $('form').submit(function(e){
-    e.preventDefault(); // prevent page reloading
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-
-  $('#refresh-data').click(function() {
-    socket.emit('get-new-data');
-    console.log("asking for data");
-  });
-
-  $('#back-button').click(function() {
-    socket.emit('get-back-data');
-    console.log("back button");
-  });
-
-  $('#next-button').click(function() {
-    socket.emit('get-next-data');
-    console.log("next button");
-  });
 })
